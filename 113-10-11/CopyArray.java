27cf4446219247cdb6357b37f2ad80befbd84501
A111222005 <a111222005@mail.shu.edu.tw>
2024-10-11 11:57:09 +0800

    113-10-11

---
+++ 113-10-11/CopyArray.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] scores1 = {88, 81, 74, 68, 78, 76, 77, 85, 95, 93};
+        int[] scores2 = Arrays.copyOf(scores1, scores1.length);
+        int[] scores3 = scores1;
+        int[] scores4 = scores1.clone();
+
+        for(var score : scores2) {
+            System.out.printf("%3d", score);
+        }
+        System.out.println();
+
+        scores2[0] = 99;
+        for(var score : scores1) {
+            System.out.printf("%3d", score);
+        }
+
+        scores3[0] = 199;
+
+        for(var score : scores1){
+            System.out.printf("%3d",score);
+        }
+
+        scores4[0] = 299;
+
+        for(var score : scores1){
+            System.out.printf("%3d",score);
+        }
+    }
+}
